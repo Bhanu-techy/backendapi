fa68577b33cf0c55c9a6e7fb0d274235eeab5993
Bhanu-techy <bhanuprakashdevari@gmail.com>
2025-10-14 08:11:57 +0000

    updated file

--- app.js
+++ app.js
@@ -82,26 +82,6 @@ app.post('/api/auth/login', async (request, response) => {
   }
 })
 
-const authenticateToken = (request, response, next) => {
-  let jwtToken
-  const authHeader = request.headers['authorization']
-  if (authHeader !== undefined) {
-    jwtToken = authHeader.split(' ')[1]
-  }
-  if (jwtToken === undefined) {
-    response.status(401)
-    response.send('Invalid JWT Token')
-  } else {
-    jwt.verify(jwtToken, 'MY_SECRET_TOKEN', async (error, payload) => {
-      if (error) {
-        response.status(401)
-        response.send('Invalid JWT Token')
-      } else {
-        next()
-      }
-    })
-  }
-}
 
 app.post('/posts', async (req, res) => {
   const {user_id, caption, img} = req.body
